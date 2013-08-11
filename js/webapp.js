5ec2bfd0dc8d29a60e38afb3e6d35b36b3a24e01
Kevin van Zonneveld <kevin@vanzonneveld.net>
2013-08-11 12:17:44 +0200

    Use real alarms

--- js/webapp.js
+++ js/webapp.js
@@ -6,7 +6,9 @@
   var pauzed           = false;
   var buttonCounter    = document.querySelector('button.counter');
   var buttonsInterval  = document.querySelectorAll('button.interval');
+  var lessonContainer  = document.querySelector('#lesson');
   var fadeFactor       = 0.6;
+  var alarmId;
 
   if (buttonCounter) {
     buttonCounter.onclick = function () {
@@ -50,17 +52,27 @@
   }
 
   var doPauze = function (want_pauze) {
+
+    // // ...
+
+    // // Later on, removing the alarm if it exists
+    // if (alarmId) {
+    //   navigator.mozAlarms.remove(alarmId);
+    // }  
+
     pauzed = want_pauze;
     if (pauzed) {
       styleBusy(false);
+      cancelAlarm();
     } else {
       styleBusy(true);
+      cancelAlarm();
+      setAlarm(currentMilliSecs);
     }
-    
+
     if ('vibrate' in navigator) {
       navigator.vibrate(200);
     }
-
   };
 
   var initCounter = function (secs) {
@@ -83,7 +95,7 @@
     // alert(formatter(activeSeconds * 1000, false) + ' Timer Complete');
 
     activeSeconds = false;
-  }
+  };
 
   var formatter = function (millisecs, with_ms) {
     var secs        = Math.floor(millisecs / 1000);
@@ -124,15 +136,53 @@
     timer = setTimeout(function () {
       doCount(secs);      
     }, milliStep);      
-  }
+  };
 
+  var setAlarm = function (ms_in_future) {
+    if (!('mozAlarms' in navigator)) {
+      return;
+    }
+
+    // var alarmDate = new Date(new Date() + currentMilliSecs);
+    alarmDate = new Date(+new Date() + ms_in_future);
+
+    // Set an alarm and store it's id
+    var request = navigator.mozAlarms.add(alarmDate, 'ignoreTimezone', {
+      message: 'Do something dude!'
+    });
+
+    request.onsuccess = function () {
+      console.log('A new alarm has been set:' + this.result);      
+      alarmId = this.result.id;
+    }  
+  };
+
+  var cancelAlarm = function() {
+    if (!('mozAlarms' in navigator)) {
+      return;
+    }
+    if (alarmId) {
+      navigator.mozAlarms.remove(alarmId);
+    }
+  };
+
+  var loadLesson = function (lesson) {
+    var xhReq = new XMLHttpRequest();
+    xhReq.open('GET', '/lessons/' + lesson + '.md', false);
+    xhReq.send(null);
+    var serverResponse = xhReq.responseText;
+    lessonContainer.innerHTML = markdown.toHTML(serverResponse);
+  };
 
   // Main
-
-  var portraitLock = screen.mozLockOrientation('portrait');
-  if (portraitLock) {
-    console.log('Orientation locked to portrait');
+  if ('mozLockOrientation' in screen) {
+    var portraitLock = screen.mozLockOrientation('portrait');
+    if (portraitLock) {
+      console.log('Orientation locked to portrait');
+    }
   }
 
   updateText(formatter(0, false));  
+  loadLesson('les2');
+
 })(); 
